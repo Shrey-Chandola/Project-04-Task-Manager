b9617f1692bd503fbbf209f0a5dcab56c7cd5c84
Shrey-Chandola <shreyashchandol@gmail.com>
2026-08-28 22:09:05 +0530

    feat: add task filtering and empty states

--- js/script.js
+++ js/script.js
@@ -36,11 +36,18 @@ function loadTasks(){  // To load all saved Tasks in page after refreshing
 // MAIN WORKING OF TASK FILTER LOGIC, DISPLAYING THEM(TASKS) ON PAGE
 function renderTasks(){  // Func. to show rendering
     console.log("Rendering tasks....");
+    console.log("current filter: ", currentFilter);
+    console.log("all tasks : ",tasks);
+// Implementing the rendering output in UI(frontend/Page) :     
+    taskList.innerHTML="";  // In HTML docu. make everything inside the taskList(our ul) empty  
+// Making whole list,buttons after the rendering is done as this is more effective way,
+//  as now we are completing rendering + UI Task Filter updation both at same time   
+
     let filteredTasks;
-    if(currentFilter==="all"){   // To show all tasks
+    if(currentFilter==="all"){
         filteredTasks = tasks;
     }
-
+    
 // filter() : It creates a new aray containing only the ele thats satisfy a cond.n
 // filter() doesn't know your cond.n beforehand. You give it a callback func.n, Means a func.n inside a func.n,
 //  & filter() supplies each array ele to that func. as the 'task' parameter
@@ -53,14 +60,16 @@ function renderTasks(){  // Func. to show rendering
     else{
         filteredTasks = tasks.filter(function(task){  // To show all completed tasks
             return task.completed===true;
+            blanckTask();
         });
     }
-    console.log(filteredTasks);  // debugging
+    console.log("filtered tasks: ",filteredTasks);  // debugging
+    if(currentFilter==="all" && filteredTasks.length===0){   // To show all tasks
+        const emptyMessage = document.createElement("p");
+        emptyMessage.textContent = "No Tasks Added Yet.";
+        taskList.appendChild(emptyMessage);
+    }
 
-// Implementing the rendering output in UI(frontend/Page) :     
-    taskList.innerHTML="";  // In HTML docu. make everything inside the taskList(our ul) empty  
-// Making whole list,buttons after the rendering is done as this is more effective way,
-//  as now we are completing rendering + UI Task Filter updation both at same time   
 
     filteredTasks.forEach(function(task){  // Run this code once for every ele in tasks array(i.e. our whole inupt tasks)
         const li = document.createElement("li");  // To make list tag(object in js) dynamically to work on it 
@@ -75,30 +84,30 @@ function renderTasks(){  // Func. to show rendering
         deleteBtn.classList.add("delete-btn");
 
         // li.textContent= inputTask.value;
-// Used span rather than original direct li.textCon.. 
-//    span.textContent = inputTask.value;  // as here through span we can add emoji's type things too
-    span.textContent = task.text;  // Now we are rendering task
+        // Used span rather than original direct li.textCon.. 
+        // span.textContent = inputTask.value;  // as here through span we can add emoji's type things too
+        span.textContent = task.text;  // Now we are rendering task
 
-    // completeBtn.textContent = "Complete";
-    completeBtn.innerHTML = '<i class="fa-solid fa-check"></i>'; // This was correct, but it was plain text only but we wanted with icons too
-    completeBtn.setAttribute("aria-label", "Mark Task as Complete");  // Thus used this 
+        // completeBtn.textContent = "Complete";
+        completeBtn.innerHTML = '<i class="fa-solid fa-check"></i>'; // This was correct, but it was plain text only but we wanted with icons too
+        completeBtn.setAttribute("aria-label", "Mark Task as Complete");  // Thus used this 
 
-    completeBtn.addEventListener("click",function(){  //After clicking on complete button completed class is assigned to it,THUS so that its styling/work can be done.  
-        task.completed = !task.completed;  // To change isCompleted/NotCompleted when we tap on complete button
-        li.classList.toggle("completed",task.completed); // toggle -> if class exists then removes it & vice-versa   
-        saveTasks();   // To save the changed data/tasks in local storage
-        renderTasks();     // To change the data accordingly that fits under complete filter  
-    });                                  
+        completeBtn.addEventListener("click",function(){  //After clicking on complete button completed class is assigned to it,THUS so that its styling/work can be done.  
+            task.completed = !task.completed;  // To change isCompleted/NotCompleted when we tap on complete button
+            li.classList.toggle("completed",task.completed); // toggle -> if class exists then removes it & vice-versa   
+            saveTasks();   // To save the changed data/tasks in local storage
+            renderTasks();     // To change the data accordingly that fits under complete filter  
+        });                                  
     
-    // deleteBtn.textContent = "Delete";  
-    deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';  
-    deleteBtn.setAttribute("aria-label", "Delete Task");
+        // deleteBtn.textContent = "Delete";  
+        deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';  
+        deleteBtn.setAttribute("aria-label", "Delete Task");
 
-    // deleteBtn.addEventListener("click",function(){
-    //     li.remove();   //It del/removes the child(here li) 
-    // });
-    deleteBtn.addEventListener("click", function(){
-        tasks = tasks.filter(function(item){
+        // deleteBtn.addEventListener("click",function(){
+        //     li.remove();   //It del/removes the child(here li) 
+        // });
+        deleteBtn.addEventListener("click", function(){
+            tasks = tasks.filter(function(item){
             return item.id!==task.id;  // To find the exact task that we marked as delete to not include in task array
         });
         saveTasks();
@@ -112,13 +121,12 @@ function renderTasks(){  // Func. to show rendering
             {count++;
             }
         }
-    taskCount.textContent = "Task Count : " + count;
+    taskCount.textContent = "Active Tasks : " + count;
 
     taskList.appendChild(li);   // Adding list & others inside their parent(taskList or say ul)  
     li.appendChild(span); // This addition does not affects the html doc. but only works  
     li.appendChild(completeBtn); // in dynamic nature.
     li.appendChild(deleteBtn);    
-
     });
 }
 
@@ -126,7 +134,8 @@ addButton.addEventListener("click",function(event){
     event.preventDefault();   // Now form/page does not refreshes,so the debuggin part could be printed and not vanished by twice refreshing of page as using form   
     if(inputTask.value.trim()==="")  //  use trim as it deletes spaces before & after of any word/text
         { return; }  // E.g: string="   data   ";  trim will make -> string  = "data";  
-        
+           console.log("Button Clicked.");  // For debugging
+    console.log(inputTask.value);
     const task = {    // Initializing the type of value of task object/array
         id: Date.now(),   // Date.now() gives the real time in hour->min->sec with no gap, decimal(.)
         text: inputTask.value,
@@ -134,11 +143,11 @@ addButton.addEventListener("click",function(event){
     };
 
     tasks.push(task);   // Adding Task(user filled by form) through task object(above) in task array
+    console.log("After push: ",tasks);
 
     saveTasks();       // To save the tasks in local storage, thus they won't vanish on page refreshing
 
-    console.log("Button Clicked.");  // For debugging
-    console.log(inputTask.value);
+ 
 
 //     const li = document.createElement("li");  // To make list tag(object in js) dynamically to work on it 
 //     const span = document.createElement("span");
